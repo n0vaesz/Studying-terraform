962b446e5c2be527888db17d325a50f1db4493fe
n0vaesz <hnovaescontato@gmail.com>
2024-01-31 01:49:15 -0300

    refactorin main.tf

--- 01-terraform-basic/main.tf
+++ 01-terraform-basic/main.tf
@@ -1,19 +1,6 @@
 #https://developer.hashicorp.com/terraform/intro
 #https://developer.hashicorp.com/terraform;language/providers
 #https://developer.hashicorp.com/terraform;language/resources
-
-
-
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
-
-# Configure the AWS Provider
 provider "aws" {
   region = "us-east-1"
   profile = "studying"
