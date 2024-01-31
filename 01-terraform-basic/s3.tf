dc81a9fc8aac5cd93942239e833ea25f0425f38e
n0vaesz <hnovaescontato@gmail.com>
2024-01-31 01:49:50 -0300

    add versioning bucket

--- 01-terraform-basic/s3.tf
+++ 01-terraform-basic/s3.tf
@@ -5,3 +5,10 @@ resource "aws_s3_bucket" "example" {
     "ManagedBy"  = "Terraform"
   }
 }
+
+resource "aws_s3_bucket_versioning" "versioning_example" {
+  bucket = aws_s3_bucket.example.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
