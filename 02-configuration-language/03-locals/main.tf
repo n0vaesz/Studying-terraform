653ff07d4f153f8b114f2a42acfb5eb4fd861b04
n0vaesz <hnovaescontato@gmail.com>
2024-02-01 23:51:32 -0300

    add main

--- 02-configuration-language/03-locals/main.tf
+++ 02-configuration-language/03-locals/main.tf
@@ -1,6 +1,7 @@
 ##https://developer.hashicorp.com/terraform/language/values/variables
+##https://developer.hashicorp.com/terraform/language/values/locals
 
 provider "aws" {
-  region  = ""
-  profile = ""
+  region  = "us-east-1"
+  profile = "studying"
 }
