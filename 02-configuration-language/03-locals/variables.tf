748e8bd916d923e8dbf8ec65f3b08f33f7f3bf25
n0vaesz <hnovaescontato@gmail.com>
2024-02-01 23:51:59 -0300

    add variables.tf

---
+++ 02-configuration-language/03-locals/variables.tf
@@ -0,0 +1,68 @@
+variable "environment" {
+  type        = string
+  description = ""
+}
+
+variable "aws_region" {
+  type        = string
+  description = ""
+  validation {
+    condition     = can(regex("^[a-z][a-z]-[a-z]+-[0-9]+$", var.aws_region))
+    error_message = "Invalid AWS region name"
+  }
+}
+
+
+
+variable "aws_profile" {
+  type        = string
+  description = "The AWS profile to use to authenticate with Terraform. Use 'default' in case you have only one account configured"
+  default     = "studying"
+}
+
+
+variable "dynamodb_field_list" {
+  type        = list(string)
+  description = ""
+  default     = ["UserId", "GameTitle"]
+}
+
+variable "database_config" {
+  type = object({
+    table_name          = string
+    read_capacity       = optional(number, 3)
+    write_capacity      = optional(number, 3)
+    deletion_protection = optional(bool, false)
+    hash_key = object({
+      name = string
+      type = string
+    })
+    range_key = object({
+      name = string
+      type = string
+    })
+  })
+  description = ""
+  default = {
+    table_name = "GameScores"
+    hash_key = {
+      name = "UserId"
+      type = "S"
+    }
+    range_key = {
+      name = "GameTitle"
+      type = "S"
+    }
+  }
+}
+
+
+variable "tags" {
+  type        = map(string)
+  description = ""
+  default = {
+    "project" = "Studying"
+
+  }
+}
+
