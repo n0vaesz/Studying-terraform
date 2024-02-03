f11ffc873410893b112835b3f25c0a4bf83c3554
n0vaesz <hnovaescontato@gmail.com>
2024-02-03 16:20:18 -0300

    add outputs.tf

---
+++ 02-configuration-language/03-outputs/outputs.tf
@@ -0,0 +1,11 @@
+output "bucket_arn" {
+  value = aws_s3_bucket.random.arn
+}
+
+output "pet_name" {
+  value = random_pet.bucket.id
+}
+
+output "bucket_domain_name" {
+  value = aws_s3_bucket.random.bucket_domain_name
+}
